1a70b4d4d80318a5e5eb489140ed7bd825390e89
Louis Nix <louis.nix2@gmail.com>
2020-08-28 23:31:23 -0500

    reconfiguring launch_app.sh script

--- launch_app.sh
+++ launch_app.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Activate virtual environment containing dependencies
-source "./venv/bin/activate"
+# Create and activate virtual environment
+python3 -m venv venv2
+source "./venv2/bin/activate"
+
+# Install dependencies to the virtual environment
+pip3 install -r requirements.txt
 
 # Set Flask variables for running app
 export FLASK_APP=top_app
 export FLASK_ENV=development
 
-# Run the app
+# Run the app locally
 pip install -e .
 flask run
