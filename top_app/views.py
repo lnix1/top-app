3d600d7aea894c101bd8c5c868384b2b630ad04e
Louis Nix <louis.nix2@gmail.com>
2020-08-29 19:41:44 -0500

    beginning to add OSM query capability

--- top_app/views.py
+++ top_app/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, flash
 from ip2geotools.databases.noncommercial import DbIpCity
+from OSMPythonTools.overpass import Overpass, overpassQueryBuilder
 
 
 bp = Blueprint('home', __name__, url_prefix='/')
@@ -10,7 +11,6 @@ def index():
 
 @bp.route('/resource_view', methods=('GET', 'POST'))
 def resource_view():
-    context = {}
 
     if request.method == 'POST':
         if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
@@ -19,11 +19,22 @@ def resource_view():
             addr = request.environ['HTTP_X_FORWARDED_FOR'] # if behind a proxy
     
     geocode = DbIpCity.get(addr, api_key='free')
+    serv_type = request.form['service_type']
 
-    context['latitude'] = geocode.latitude
-    context['longitude'] = geocode.longitude
-    context['service_type'] = request.form['service_type']
+    query = overpassQueryBuilder(bbox=[geocode.latitude-10, geocode.longitude-10, 
+                                       geocode.latitude+10, geocode.longitude+10],
+                             elementType='node',
+                             selector='"building"='+str(serv_type)+'"',
+                             includeGeometry=False)
+
+    result = overpass.query(query)
+    flash(result.elements()[0].tags('name'))
+    flash(result.elements()[0].tags('addr:city'))
+
+    #context = {}
+    #context['latitude'] = geocode.latitude
+    #context['longitude'] = geocode.longitude
+    #context['service_type'] = request.form['service_type']
+    #flash(context)
     
-    flash(context)
-    
-    return render_template('resource_view.html', **context)
+    return render_template('resource_view.html'}#, **context)
