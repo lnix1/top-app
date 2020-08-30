e38be0849744019bc2b4ee6e24fcf140a98fa7f4
Louis Nix <louis.nix2@gmail.com>
2020-08-29 20:21:13 -0500

    testing things

--- top_app/views.py
+++ top_app/views.py
@@ -29,8 +29,8 @@ def resource_view():
 
     overpass = Overpass()
     result = overpass.query(query)
-    flash(result.elements()[0].tag('name'))
-    flash(result.elements()[0].tag('addr:city'))
+    flash(result.elements()[21].tag('name'))
+    flash(result.elements()[21].tag('addr:city'))
 
     #context = {}
     #context['latitude'] = geocode.latitude
