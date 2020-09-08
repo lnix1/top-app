0e02a392a02a7b748d17b8b6098b167d0a0bd73a
nicoledan <nicole_k_dan@hotmail.com>
2020-09-07 20:15:31 -0400

    style updates

--- top_app/views.py
+++ top_app/views.py
@@ -11,13 +11,16 @@ def index():
 
 @bp.route('/resource_view', methods=('GET', 'POST'))
 def resource_view():
-
+    # comment out to hardcode ip address
     if request.method == 'POST':
         if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
             addr = request.environ['REMOTE_ADDR']
         else:
             addr = request.environ['HTTP_X_FORWARDED_FOR'] # if behind a proxy
     
+    #put known ip address in brackets
+    addr = []
+
     geocode = DbIpCity.get(addr, api_key='free')
     serv_type = request.form['service_type']
 
