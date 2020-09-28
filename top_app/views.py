83d762a8fdb3bd711027e2e61599fda0fcae3314
Louis Nix <louis.nix2@gmail.com>
2020-09-28 00:21:43 -0500

    adding initial tool

--- top_app/views.py
+++ top_app/views.py
@@ -4,7 +4,7 @@ from OSMPythonTools.overpass import Overpass, overpassQueryBuilder
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
-@bp.route('')
+@bp.route('/')
 def index():
     return render_template('index.html')
 
@@ -16,7 +16,8 @@ def resource_view():
             addr = request.environ['REMOTE_ADDR']
         else:
             addr = request.environ['HTTP_X_FORWARDED_FOR'] # if behind a proxy
-            
+
+    #addr = "98.163.214.113"       
     geocode = DbIpCity.get(addr, api_key='free')
     serv_type = request.form['service_type']
 
@@ -56,3 +57,31 @@ def resource_view():
     }
     
     return render_template('resource_view.html', context = context, coords = [geocode.latitude, geocode.longitude])
+
+@bp.route('/lookup', methods=('GET', 'POST'))
+def lookup():
+    return render_template('lookup.html')
+
+@bp.route('/tagging', methods=('GET', 'POST'))
+def tagging():
+    tag_address = request.form['yes_no']
+    return render_template('tagging.html', context = tag_address)
+
+@bp.route('write_osm', methods=('GET', 'POST'))
+def write_osm():
+    context = {
+        'org-name' : request.form['org-name'],
+        'services' : request.form['services'],
+        'r-services' : request.form['r-services'],
+        'website' : request.form['website'],
+        'p-number' : request.form['pnumber']
+    }
+
+    # utilizing the 'osmapi' package to write to OSM servers. Package info here: https://pypi.org/project/osmapi/
+    #import osmapi
+    #api = osmapi.OsmApi(api="https://api06.dev.openstreetmap.org", username = u"metaodi", password = u"*******")
+    #api.ChangesetCreate({u"comment": u"My first test"})
+    #print(api.NodeCreate({u"lon":1, u"lat":1, u"tag": {}}))
+    #api.ChangesetClose()
+
+    return render_template('/lookup.html')
