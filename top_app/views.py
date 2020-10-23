299b81db1e1326d2d03febee59b2e61592b18c19
Louis Nix <louis.nix2@gmail.com>
2020-10-23 01:09:55 -0500

    adding login and registration pages

--- top_app/views.py
+++ top_app/views.py
@@ -1,12 +1,95 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, g, redirect, session, url_for
 from ip2geotools.databases.noncommercial import DbIpCity
 from OSMPythonTools.overpass import Overpass, overpassQueryBuilder
+from werkzeug.security import check_password_hash, generate_password_hash
+import functools
+from top_app.db import get_db
 
 bp = Blueprint('home', __name__, url_prefix='/')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('home.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
 
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('home.login'))
+
+        flash(error)
+
+    return render_template('register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('home.index'))
+
+        flash(error)
+
+    return render_template('login.html')
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home.index'))
 
 @bp.route('/resource_view', methods=('GET', 'POST'))
 def resource_view():
@@ -59,15 +142,18 @@ def resource_view():
     return render_template('resource_view.html', context = context, coords = [geocode.latitude, geocode.longitude])
 
 @bp.route('/lookup', methods=('GET', 'POST'))
+@login_required
 def lookup():
     return render_template('lookup.html')
 
 @bp.route('/tagging', methods=('GET', 'POST'))
+@login_required
 def tagging():
     tag_address = request.form['yes_no']
     return render_template('tagging.html', context = tag_address)
 
 @bp.route('write_osm', methods=('GET', 'POST'))
+@login_required
 def write_osm():
     context = {
         'org-name' : request.form['org-name'],
