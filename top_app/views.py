68c18bee7fb3e7fadef309bc10079b9d1301c911
Louis Nix II <louis.nix2@gmail.com>
2020-10-26 18:08:08 -0500

    messed up the push to heroku

--- top_app/views.py
+++ top_app/views.py
@@ -90,12 +90,12 @@ def logout():
 @bp.route('/resource_view', methods=('GET', 'POST'))
 def resource_view():
     # comment out to hardcode ip address
-    #if request.method == 'POST':
-    #    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
-    #        addr = request.environ['REMOTE_ADDR']
-    #    else:
-    #        addr = request.environ['HTTP_X_FORWARDED_FOR'] # if behind a proxy
-    addr ="76.176.72.174"
+    if request.method == 'POST':
+        if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+            addr = request.environ['REMOTE_ADDR']
+        else:
+            addr = request.environ['HTTP_X_FORWARDED_FOR'] # if behind a proxy
+    #addr ="76.176.72.174"
 
     #addr = "98.163.214.113"       
     geocode = DbIpCity.get(addr, api_key='free')
