800c9b2fec6540ec7f66f0b88e1493462399202d
Louis Nix II <louis.nix2@gmail.com>
2020-10-25 22:40:55 -0500

    now i am fixing the data base issue

--- top_app/db.py
+++
@@ -1,9 +0,0 @@
-import click
-from flask import current_app, g
-from flask.cli import with_appcontext
-
-def get_db():
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/top_app'
-    db = SQLAlchemy(app)
-
-    return db
