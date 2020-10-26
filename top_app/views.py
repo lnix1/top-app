ac391f226ade0c06c6f0fcab7f34df98880698a1
Louis Nix II <55001608+lnix1@users.noreply.github.com>
2020-10-26 01:36:20 -0500

    Revert "Nicole and Xinyi updates 10/25"

--- top_app/views.py
+++ top_app/views.py
@@ -1,9 +1,6 @@
 from flask import Blueprint, render_template, request, flash, g, redirect, session, url_for
 from ip2geotools.databases.noncommercial import DbIpCity
 from OSMPythonTools.overpass import Overpass, overpassQueryBuilder
-import requests
-from xml.etree import ElementTree
-from itertools import chain
 from werkzeug.security import check_password_hash, generate_password_hash
 import functools
 from top_app import db, User
@@ -95,77 +92,32 @@ def resource_view():
             addr = request.environ['REMOTE_ADDR']
         else:
             addr = request.environ['HTTP_X_FORWARDED_FOR'] # if behind a proxy
-    #addr ="76.176.72.174"
 
     #addr = "98.163.214.113"       
     geocode = DbIpCity.get(addr, api_key='free')
     serv_type = request.form['service_type']
-    overpass = Overpass()
 
-    if (serv_type in ['place_of_worship','childcare']):
-        query1 = overpassQueryBuilder(bbox=[geocode.latitude-1, geocode.longitude-1, 
-                                        geocode.latitude+1, geocode.longitude+1],
-                                    elementType=['node','way'],
-                                    selector=['"amenity"="'+str(serv_type)+'"','name'],
-                                    includeGeometry=False)
-        
-        result1 = overpass.query(query1).toJSON()
-        result1 = result1['elements']
-        query2 = overpassQueryBuilder(bbox=[geocode.latitude-1, geocode.longitude-1, 
-                                        geocode.latitude+1, geocode.longitude+1],
-                                    elementType=['node'],
-                                    selector=['"amenity"="kindergarten"','name'],
-                                    includeGeometry=False)
-        result2 = overpass.query(query2).toJSON()
-        result2 = result2['elements']
-        result0 = [result1,result2]
-        result = list(chain.from_iterable(result0))
-    elif (serv_type in ['place_of_worship','hospital','bank']):
+    if (serv_type in ['hospital', 'place_of_worship','bank']):
         query = overpassQueryBuilder(bbox=[geocode.latitude-1, geocode.longitude-1, 
                                         geocode.latitude+1, geocode.longitude+1],
                                     elementType='node',
-                                    selector=['"amenity"="'+str(serv_type)+'"','name'],
+                                    selector=['"amenity"="'+str(serv_type)+'"'],
                                     includeGeometry=False)
-        result = overpass.query(query).toJSON()
-        result = result['elements']
     elif (serv_type in ['mental_health']):
         query = overpassQueryBuilder(bbox=[geocode.latitude-10, geocode.longitude-10, 
                                         geocode.latitude+10, geocode.longitude+10],
-                                    elementType=['node','way'],
-                                    selector=['"social_facility:for"="'+str(serv_type)+'"','name'],
+                                    elementType='node',
+                                    selector=['"social_facility:for"="'+str(serv_type)+'"'],
                                     includeGeometry=False)
-        result = overpass.query(query).toJSON()
-        result = result['elements']
     else:
         query = overpassQueryBuilder(bbox=[geocode.latitude-1, geocode.longitude-1, 
                                         geocode.latitude+1, geocode.longitude+1],
-                                    elementType=['node','way'],
-                                    selector=['"office"="'+str(serv_type)+'"','name'],
+                                    elementType='node',
+                                    selector=['"office"="'+str(serv_type)+'"'],
                                     includeGeometry=False)
-        result = overpass.query(query).toJSON()
-        result = result['elements']
-    
-    for element in result:
-        if element['type']=='way':
-            apiquery = requests.get("https://api.openstreetmap.org/api/0.6/node/"+str(element['id']))
-            if apiquery.content != b'':
-                apitree = ElementTree.fromstring(apiquery.content)
-                for child in apitree.iter('node'):
-                    element['cord'] = [float(child.attrib['lon']),float(child.attrib['lat'])]
-                element['type'] ='Point'
-            else:
-                length = len(element['nodes'])
-                element['cord']=list(range(0,length))
-                for idnum in range(0,length):
-                    apiquery2 = requests.get("https://api.openstreetmap.org/api/0.6/node/"+str(element['nodes'][idnum]))
-                    apitree2 = ElementTree.fromstring(apiquery2.content)
-                    for child in apitree2.iter('node'):
-                        element['cord'][idnum] = [float(child.attrib['lon']),float(child.attrib['lat'])]   
-                element['type'] ='LineString'
-        else:
-            element['type'] ='Point'
-            element['cord'] = [element['lon'],element['lat']]
 
+    overpass = Overpass()
+    result = overpass.query(query).toJSON()
     
     context = {
         "type": "FeatureCollection",
@@ -174,10 +126,10 @@ def resource_view():
             "type": "Feature",
             "properties" : d['tags'],
             "geometry" : {
-                "type": d['type'],
-                "coordinates": d['cord'],
+                "type": "Point",
+                "coordinates": [d["lon"], d["lat"]],
                 },
-        } for d in result]
+        } for d in result['elements']]
     }
     
     return render_template('resource_view.html', context = context, coords = [geocode.latitude, geocode.longitude])
